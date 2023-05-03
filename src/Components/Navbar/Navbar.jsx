f1c0ad6f882043bcd2218e4ef900e191d3204ecf
Fmajid29 <faizanmajid11@gmail.com>
2023-05-03 09:11:33 +0500

    started working on Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return <div className="MainContainer">Navbar</div>;
+};
+
+export default Navbar;
--- src/Components/Tables/Tables.jsx
+++ src/Components/Tables/Tables.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 const Tables = () => {
-  return <div>hello</div>;
+  return (
+    <>
+      <Navbar />
+      <div>hello</div>;
+    </>
+  );
 };
 
 export default Tables;
