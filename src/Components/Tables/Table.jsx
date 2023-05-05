55fd60f0ffa5741d3ecb9300cd06bb251f3fc187
Fmajid29 <faizanmajid11@gmail.com>
2023-05-05 11:45:30 +0500

    starting the correction of bill

--- src/Components/Tables/Table.jsx
+++ src/Components/Tables/Table.jsx
@@ -5,7 +5,10 @@ import { useNavigate } from "react-router";
 const Table = (props) => {
   const navigate = useNavigate();
   const [tableState, settablestate] = useState(props.tableState);
+
   function handleClick() {
+    settablestate("serving");
+    console.log(tableState);
     navigate("/category");
   }
 
